c0f10eb0b304157cb258cb7ff5a2fde8237d14c6
rob schieber <robert.schieber@jpmchase.com>
2012-10-20 11:10:31 -0400

    added ability to specify host in options

--- test/test.js
+++ test/test.js
@@ -2,21 +2,14 @@ describe('mockey', function () {
     var should = require("should"),
         assert = require('assert'),
         colors = require('colors'),
-        NodeMockey = require('../index');
-    var mockey = new NodeMockey('localhost:8080');
+        mockey = require('../index').create('http://localhost:8080');
     describe('#configureService()', function () {
 
         it('should return a response', function (done) {
             this.timeout(1000);
             mockey.configureService({
                 serviceName:'Auth',
-                scenerioName:'invalid',
-                serviceResponseType:'static'
-                //serviceId:0,
-                //scenerioId:0,
-                //hangTime:0,
-                //transientState:true,
-
+                scenarioName:'invalid'
             }, function (response) {
                 assert.equal(response.serviceName, 'Auth');
                 done();
